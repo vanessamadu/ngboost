2fedac8a00ea42588b14429c94576e84dd04f7a3
vanessamadu <vanessa.madu18@imperial.ac.uk>
2026-08-14 15:51:42 +0100

    add properties

--- ngboost/distns/multivariate_skew_t.py
+++ ngboost/distns/multivariate_skew_t.py
@@ -3,6 +3,10 @@
 from ngboost.distns.distn import RegressionDistn
 from ngboost.scores import LogScore
 
+from scipy.special import gamma
+from scipy.stats import t, multivariate_normal, chi2
+import numpy as np
+
 class MVStLogScore(LogScore):
     def score(self, Y):
         return -self.logpdf(Y)
@@ -33,10 +37,10 @@ class MVStLogScore(LogScore):
         pass
 
 
-def MultivariateSkewT(k):
+def MultivariateSkewT(d):
     """
     #  Factory function that generates classes for
-    #  k-dimensional multivariate skew-t distributions for NGBoost
+    #  d-dimensional multivariate skew-t distributions for NGBoost
 
     # This distribution has LogScore implemented for it.
 
@@ -47,22 +51,55 @@ def MultivariateSkewT(k):
 
         """
 
-        n_params = None
+        n_params = int(1 + d * (d + 5) / 2)
         scores = [MVStLogScore]
         multi_output = True
 
         def __init__(self, params):
             super().__init__(params)
-            pass
+
+            self.d = d
+
+            self.nu0 = None
+            self.nu_tilde = None
+            self.rho = None
+            self.loc = None
+            self.eta = None
 
         def logpdf(self, Y):
-            pass
+
+            term1 = np.log( 
+                gamma( 
+                    (self.df + d) / 2
+                ) / (
+                gamma(
+                    self.df / 2
+                ) * (np.pi * self.df) ** (d / 2)
+                )
+            )
+
+            term2 = np.sum(self.rho) / 4
+
+            term3 = - (self.df / 2) * (1 + d / self.df) * np.log(1 + self.Q(Y) / self.df)
+
+            term4 = np.log(2 * t.cdf(
+                np.sqrt(self.df + d / 
+                        self.df + self.Q(Y)) * np.dot(
+                            self.eta, Y - self.loc
+                        )
+            ), df = self.df + d)
+
+            return term1 + term2 + term3 + term4
 
         def fit(Y):
             pass
 
         def rv(self):
-            pass
+            u_star = multivariate_normal(0, self.omega_star).rvs(size=self.d + 1)
+            v = chi2(df = self.df).rvs() / self.df
+            z = np.matmul(self.stds, u_star[1:]) * np.sign(u_star[0])
+            return self.loc + z / np.sqrt(v)
+
 
         def rvs(self, n):
             return [self.rv() for _ in range(n)]
@@ -73,6 +110,46 @@ def MultivariateSkewT(k):
         @property
         def disp(self):
             pass
+
+        @property
+        def stds(self):
+            pass
+
+        @property
+        def corr(self):
+            pass
+
+        @property
+        def omega_star(self):
+            pass
+
+        @property
+        def df(self):
+            return self.nu0 + np.exp(self.nu_tilde)
+
+        @property
+        def skew(self):
+            pass
+
+        def Q(self, Y):
+            return np.matmul(
+                np.matmul(Y - self.loc , self.disp),
+                np.transpose(Y - self.loc)
+            )
+
+        @property
+        def delta(self):
+            np.matmul(
+                self.corr, self.skew
+                ) / (
+                    1 + np.sqrt(
+                        np.matmul(
+                            np.matmul(
+                                np.transpose(self.skew),
+                                self.corr),
+                        self.skew)
+                    )
+                )
 
         @property
         def params(self):
